e0914700aa0d71062df0c1fe6a5a634c7f186807
SSashinn <sayedayaan226@gmail.com>
2023-06-13 18:19:36 +0530

    Added the comparision and scores.

--- index.js
+++ index.js
@@ -1,16 +1,85 @@
-let computerGuess = compGuess();
-console.log(computerGuess);
-let playerGuess = userGuess();
-console.log(playerGuess); 
+const choices=["rock","paper","scissors"];
+let playerScore = 0;
+let compScore = 0;
+let tie = 0;
+main();
+
+
+function main(){
+    for (let i = 0; i<5; i++){
+        let computerGuess = compGuess();
+        // console.log("Computer guess is",computerGuess);
+    
+        let playerGuess = userGuess();
+        // console.log(playerGuess);
+
+        console.log("Comp guess is",computerGuess);
+        console.log(result(computerGuess, playerGuess));
+    }
+    console.log("comp score:",compScore,"\nPlayer score:",playerScore,"\nTies:",tie);
+}
 
 
 function compGuess(){
-    const choices=["rock","paper","scissors"];
     let index = Math.floor(Math.random()*3);
     return choices[index];
 }
 
+
 function userGuess(){
-    let userChoice = prompt("Choose your fighter!");
-    return userChoice.toLowerCase()
+    while (true){
+        let userChoice = prompt("Choose your fighter!");
+        userChoice = userChoice.toLowerCase()
+        for (let i = 0; i<3; i++){
+            if (choices[i] == userChoice){
+                return userChoice;
+            }
+        }
+        console.log("INVALID INPUT!!");
+    }
+}
+
+
+function result(computerGuess, playerGuess){
+    if (computerGuess == playerGuess){
+        tie++;
+        return "It was a tie!!"
+    }
+    else if (computerGuess=='rock'){
+        switch (playerGuess) {
+            case "paper":
+                playerScore++
+                return "You WON!!";
+            case "scissors":
+                compScore++
+                return "You LOSE!!";
+            default:
+                return -1;
+        }
+    }
+    else if (computerGuess=='paper'){
+        switch (playerGuess) {
+            case "scissors":
+                playerScore++
+                return "You WON!!";
+            case "rock":
+                compScore++
+                return "You LOSE!!";
+            default:
+                return -1;
+        }
+    }
+    else if (computerGuess=='scissors'){
+        switch (playerGuess) {
+            case "rock":
+                playerScore++
+                return "You WON!!";
+            case "paper":
+                compScore++
+                return "You LOSE!!";
+            default:
+                return -1;
+        }
+    }
+    
 }
