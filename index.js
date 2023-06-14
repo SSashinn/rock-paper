f352bf734959a8dbf892a8dd7c59080a7168a0aa
SSashinn <sayedayaan226@gmail.com>
2023-06-14 23:55:52 +0530

    Created buttons

--- index.js
+++ index.js
@@ -2,47 +2,55 @@ const choices=["rock","paper","scissors"];
 let playerScore = 0;
 let compScore = 0;
 let tie = 0;
-main();
+let user = userGuess();
+let comp = compGuess();
+document.querySelector('.start').onclick = main;
 
 
 function main(){
-    for (let i = 0; i<5; i++){
-        let computerGuess = compGuess();    
-        let playerGuess = userGuess();
-
-        console.log("Comp guess is",computerGuess);
-        console.log(result(computerGuess, playerGuess));
-    }
+    let playerGuess = document.querySelector('#userGuess').textContent;
+    let computerGuess = document.querySelector('#compGuess').textContent; 
+    console.log(result(computerGuess, playerGuess));
+    document.getElementById('compGuess').style.visibility = 'visible'; 
     console.log("Player score:",playerScore,"\ncomp score:",compScore,"\nTies:",tie);
 }
 
 
 function compGuess(){
     let index = Math.floor(Math.random()*3);
-    return choices[index];
+    document.querySelector('#compGuess').textContent = choices[index];
 }
+
+
+// function userGuess(){
+// //     while (true){
+// //         let userChoice = prompt("Choose your fighter!");
+// //         userChoice = userChoice.toLowerCase()
+// //         for (x of choices){
+// //             if (x == userChoice){
+// //                 return userChoice;
+// //             }
+// //         }
+// //         console.log("INVALID INPUT!!");
+// //     }
+// // }
 
 
 function userGuess(){
-    while (true){
-        let userChoice = prompt("Choose your fighter!");
-        userChoice = userChoice.toLowerCase()
-        for (x of choices){
-            if (x == userChoice){
-                return userChoice;
-            }
-        }
-        console.log("INVALID INPUT!!");
-    }
-}
+    document.querySelectorAll('button').forEach(function(button) {
+        button.onclick = function(){
+            document.querySelector('div').textContent = button.dataset.input;
+        } 
+    });
+};
 
 
 function result(computerGuess, playerGuess){
-    if (computerGuess == playerGuess){
+    if (computerGuess === playerGuess){
         tie++;
         return "It was a tie!!"
     }
-    else if (computerGuess=='rock'){
+    else if (computerGuess==='rock'){
         switch (playerGuess) {
             case "paper":
                 playerScore++
@@ -54,7 +62,7 @@ function result(computerGuess, playerGuess){
                 return -1;
         }
     }
-    else if (computerGuess=='paper'){
+    else if (computerGuess==='paper'){
         switch (playerGuess) {
             case "scissors":
                 playerScore++
@@ -66,7 +74,7 @@ function result(computerGuess, playerGuess){
                 return -1;
         }
     }
-    else if (computerGuess=='scissors'){
+    else if (computerGuess==='scissors'){
         switch (playerGuess) {
             case "rock":
                 playerScore++
